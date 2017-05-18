ca6814b15957a8ccb0a2d2fe5499473902e22642
Jakub Janusz <dr.drzuby@gmail.com>
2017-05-18 16:48:43 +0200

    Added deserializing BioWIZ project.

--- spring_boot/src/main/java/greeting/robot/server/Application.java
+++ spring_boot/src/main/java/greeting/robot/server/Application.java
@@ -2,8 +2,10 @@ package greeting.robot.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"greeting.robot.server", "pl.edu.agh.biowiz"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- spring_boot/src/main/java/greeting/robot/server/DescriptorService.java
+++ spring_boot/src/main/java/greeting/robot/server/DescriptorService.java
@@ -1,15 +1,33 @@
 package greeting.robot.server;
 
 import greeting.robot.data.api.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.edu.agh.biowiz.model.Project;
 import pl.edu.agh.biowiz.model.profile.PwFaceDescriptor;
+import pl.edu.agh.biowiz.repo.holder.ProjectHolder;
 
+import javax.annotation.PostConstruct;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DescriptorService {
 
+    @Autowired
+    private ProjectHolder projectHolder;
+    private Project biowizProject;
+
+    @PostConstruct
+    public void postConstruct() {
+        Optional<Project> project = projectHolder.openProject(".biowiz-data/exampleCelebrites/project.biow");
+        if (project.isPresent()) {
+            biowizProject = project.get();
+        } else {
+            throw new IllegalStateException("Cannot deserialize project");
+        }
+    }
 //    private AnalyserService analyserService;
 
 //    @Autowired
